1d4f147266b6ab7c84303cc0ff0d30b9d938ded8
ahmed belal <legendb@gmail.com>
2025-05-09 18:22:11 +0300

    some changes in userinfo

--- Uni_Mate/Features/ApartmentManagment/Rooms/RoomBedViewModel.cs
+++ Uni_Mate/Features/ApartmentManagment/Rooms/RoomBedViewModel.cs
@@ -4,11 +4,27 @@ namespace ApartmentManagment.Features.ApartmentManagment.Rooms;
 
 public record BedViewModel(bool IsAvailable, double Price);
 public record RoomBedViewModel(string Description, int Price, string Image, List<BedViewModel> Beds);
-public class RoomBedViewModelValidator : AbstractValidator<List<RoomBedViewModel>
+public class RoomBedViewModelValidator : AbstractValidator<List<RoomBedViewModel>>
 {
     public RoomBedViewModelValidator()
     {
         RuleForEach(x => x)
-            .SetValidator(new RoomBedViewModelValidator());
+            .SetValidator(new RoomBedViewModeleValidator());
+    }
+}
+
+public class RoomBedViewModeleValidator : AbstractValidator<RoomBedViewModel>
+{
+    public RoomBedViewModeleValidator()
+    {
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0.");
+        RuleFor(x => x.Image)
+            .NotEmpty().WithMessage("Image is required.");
+        RuleFor(x => x.Beds)
+            .NotEmpty().WithMessage("At least one bed is required.");
     }
 }
