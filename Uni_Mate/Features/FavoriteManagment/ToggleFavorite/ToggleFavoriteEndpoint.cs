9c07fb090354f503dfdda1f2f688ce18c32f3dbe
Ziad Atta <ziadatta2233@gmail.com>
2025-05-24 15:53:56 +0300

    The Is Toggel Endpoint

---
+++ Uni_Mate/Features/FavoriteManagment/ToggleFavorite/ToggleFavoriteEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Uni_Mate.Common.BaseEndpoints;
+using Uni_Mate.Common.Views;
+using Uni_Mate.Features.FavoriteManagment.ToggleFavorite.Command;
+
+namespace Uni_Mate.Features.FavoriteManagment.ToggleFavorite
+{
+    [Authorize]
+    public class ToggleFavoriteEndpoint : BaseEndpoint<ToggleFavoriteVM,bool>
+    {
+        public ToggleFavoriteEndpoint(BaseEndpointParameters<ToggleFavoriteVM> parameters) : base(parameters)
+        {
+        }
+        [HttpPost]
+        
+        public async Task<EndpointResponse<bool>> ToggleFavorite([FromBody] ToggleFavoriteVM request)
+        {
+            // check on the validator 
+            var valid = ValidateRequest(request);
+            if (valid.isSuccess == false)
+            {
+                return EndpointResponse<bool>.Failure(valid.errorCode, valid.message);
+            }
+
+            var result = await _mediator.Send(new ToggleFavoriteCommand(request.id));
+            if (result.isSuccess)
+            {
+                return EndpointResponse<bool>.Success(result.data, result.message);
+            }
+            return EndpointResponse<bool>.Failure(result.errorCode, result.message);
+        }
+
+    }
+}
