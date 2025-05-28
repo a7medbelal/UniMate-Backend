6dfc31c9136d510e3fe3a3139baae54608220e2f
Hossam Hassan <hossamh.abo.elsoud@gmail.com>
2025-05-28 19:43:31 +0300

    added update profile picture

---
+++ Uni_Mate/Features/Common/UserProfilePicture/Commands/DeleteProfilePictureCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Uni_Mate.Common.BaseHandlers;
+using Uni_Mate.Common.Data.Enums;
+using Uni_Mate.Common.Views;
+using Uni_Mate.Features.Common.DeleteImage.Commands;
+using Uni_Mate.Models.UserManagment;
+
+namespace Uni_Mate.Features.Common.UserProfilePicture.Commands;
+public record DeleteProfilePictureCommand() : IRequest<RequestResult<bool>>;
+
+public class DeleteProfilePictureCommandHandler : BaseWithoutRepositoryRequestHandler<DeleteProfilePictureCommand, RequestResult<bool>, User>
+{
+    public DeleteProfilePictureCommandHandler(BaseWithoutRepositoryRequestHandlerParameters<User> parameters) : base(parameters)
+    {
+    }
+
+    public async override Task<RequestResult<bool>> Handle(DeleteProfilePictureCommand request, CancellationToken cancellationToken)
+    {
+        //var user = await _userManager.FindByIdAsync(_userInfo.ID);
+        var user = await _userManager.FindByIdAsync("1");
+        string? oldImage = user.Image;
+
+        if (oldImage == null || oldImage == "")
+        {
+            return RequestResult<bool>.Failure(ErrorCode.NotFound, "Old image not found");
+        }
+        user.Image = null;
+
+        var deleteImageCommand = new DeleteImageCommand(oldImage);
+        var deleteResult = await _mediator.Send(deleteImageCommand, cancellationToken);
+        if (!deleteResult.isSuccess)
+        {
+            return RequestResult<bool>.Failure(ErrorCode.DeleteFailed, "Failed to delete old image");
+        }
+        return RequestResult<bool>.Success(true, "Message deleted successfully");
+    }
+}
---
+++ Uni_Mate/Features/Common/UserProfilePicture/UploadProfilePictureViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Uni_Mate.Features.Common.UserProfilePicture;
+public record UploadProfilePictureViewModel(IFormFile Image);
+public class UploadProfilePictureVieModelValidator : AbstractValidator<UploadProfilePictureViewModel>
+{
+    public UploadProfilePictureVieModelValidator()
+    {
+        RuleFor(x => x.Image)
+            .Must(file => file.Length <= 5 * 1024 * 1024) // 5 MB limit
+            .WithMessage("Image size must not exceed 5 MB.");
+    }
+}
