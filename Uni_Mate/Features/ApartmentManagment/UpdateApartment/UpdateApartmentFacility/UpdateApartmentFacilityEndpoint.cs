5c9363dddd2b3e3ed2ef54c5a7d287874c65c47f
Hossam Hassan <hossamh.abo.elsoud@gmail.com>
2025-05-29 14:45:01 +0300

    added update apartment facility

---
+++ Uni_Mate/Features/ApartmentManagment/UpdateApartment/UpdateApartmentFacility/UpdateApartmentFacilityEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+using Uni_Mate.Common.BaseEndpoints;
+using Uni_Mate.Common.Views;
+using Uni_Mate.Features.ApartmentManagment.CreateApartmnetProcess.Commands.CategoryWithFaciltyCommand;
+using Uni_Mate.Features.ApartmentManagment.UpdateApartment.UpdateApartmentFacility.Commands;
+using Uni_Mate.Models.ApartmentManagement;
+
+namespace Uni_Mate.Features.ApartmentManagment.UpdateApartment.UpdateApartmentFacility;
+public class UpdateApartmentFacilityEndpoint : BaseEndpoint<List<FacilityApartmentViewModel>, bool>
+{
+    public UpdateApartmentFacilityEndpoint(BaseEndpointParameters<List<FacilityApartmentViewModel>> parameters) : base(parameters)
+    {
+    }
+
+    [HttpPost]
+    public async Task<EndpointResponse<bool>> UpdateApartmentFacility([FromBody] List<FacilityApartmentViewModel> viewmodel)
+    {
+        var validationResult = ValidateRequest(viewmodel);
+        if (!validationResult.isSuccess)
+        {
+            return validationResult;
+        }
+        var command = new UpdateApartmentFacilityCommand(viewmodel, 3);
+        var result = await _mediator.Send(command);
+        if (!result.isSuccess)
+        {
+            return EndpointResponse<bool>.Failure(result.errorCode, result.message);
+        }
+        return EndpointResponse<bool>.Success(result.data, "Apartment facilities updated successfully.");
+    }
+}
