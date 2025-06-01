83e1b4365b173b67f542e965a7f98cca468917c2
Ziad Atta <ziadatta2233@gmail.com>
2025-06-01 06:35:58 +0300

    The Folder Name IS Extra Endpoints

---
+++ Uni_Mate/Features/ExtraEndpoints/GetOwnerById/GetOwnerByIdQuarry/GetOwnerByIdDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Uni_Mate.Features.ExtraEndpoints.GetOwnerById.GetOwnerByIdQuarry
+{
+    public class GetOwnerByIdDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? FName { get; set; } = string.Empty;
+        public string? LName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string NationalId { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string? Governomet { get; set; }
+    }
+}
